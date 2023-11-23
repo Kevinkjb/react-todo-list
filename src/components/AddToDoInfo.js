55ba4b8d7fb99d636fd3a42f47bd609d394831af
Kevin Baguioso <tomfge09@yahoo.ca>
2023-11-22 21:49:14 -0700

    Initial files has been updated

--- src/components/AddToDoInfo.js
+++ src/components/AddToDoInfo.js
@@ -1,14 +1,19 @@
+import {useState} from 'react'
+
 export const AddToDoInfo = ({item, deleteList}) =>{
   // const currentTime = new Date().toLocaleTimeString()
+  const [toggle, setToggle] = useState(false)
+
   return(
     <div className="mx-auto h-10 w-96">
       <div className="flex justify-between items-center my-2">
         <div className="flex items-center">
-          <input type="checkbox" 
+          <input type="checkbox"
+          onChange={()=> setToggle(!toggle)} 
           className="mr-2 h-4 w-4 rounded border-gray-300 focus:ring-indigo-500 text-indigo-600"
           />
           <div className="flex items-center justify-between w-72">
-          <p className="text-gray-500 text-sm">{item.taskList}</p>
+          <p className="text-blue-500 text-sm" style={toggle ? {textDecorationLine: "line-through", color: "gray"}:{textDecorationLine: "none"}}>{item.taskList }</p>
           {/* <p className="text-xs text-blue-400">{currentTime}</p> */}
           </div>
         </div>
